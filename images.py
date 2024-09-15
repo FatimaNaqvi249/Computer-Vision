9eedae7286508f9c41d08caa66b3ebab531f676a
FatimaNaqvi249 <128723826+FatimaNaqvi249@users.noreply.github.com>
2024-09-15 15:47:36 +0500

    Update images.py

--- images.py
+++ images.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Load an image from the Images folder
-image = cv2.imread('Images/image1.jpg')
+image = cv2.imread('Images/ss1.png')
 
 # Display the image in a window
 cv2.imshow('Loaded Image', image)
@@ -9,4 +9,3 @@ cv2.imshow('Loaded Image', image)
 # Wait for any key press and close the window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
